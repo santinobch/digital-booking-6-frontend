39457bd6e36844c31020ca3642ec79ed5c609709
Santino <santino.bch@gmail.com>
2022-10-24 20:45:47 -0300

    Commit cambios

---
+++ src/App/components/drawer/drawer.js
@@ -0,0 +1,45 @@
+import facebook from '../../../imgs/socials/facebook.png';
+import linkedin from '../../../imgs/socials/linkedin.png';
+import twitter from '../../../imgs/socials/tweet.png';
+import instagram from '../../../imgs/socials/ig.png';
+
+import styles from "./drawer.module.scss";
+import { IconContext } from "react-icons";
+
+import { FaFacebook } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+export default function Drawer() {
+    return (
+
+        <IconContext.Provider value={{ color: "#31363F", size:"20px" }}>
+            <div className={styles.drawer}>
+                <div className={styles.drawerTop}>
+                    <p>X</p>
+                    <h1>MENÚ</h1>
+                </div>
+
+                <div className={styles.drawerBottom}>
+                    <div className={styles.controls}>
+                        <div className={styles.controlsTop}>
+                            <p>Crear cuenta</p>
+                            <p>Iniciar sesión</p>
+                        </div>
+                        <div className={styles.controlsBottom}>
+                            <p>¿Deseas <a href="">cerrar sesión</a>?</p>
+                        </div>
+                    </div>
+                    <div className={styles.links}>
+                        <a id="facebook" href=""><FaFacebook/></a>
+                        <a id="linkedin" href=""><FaLinkedinIn/></a>
+                        <a id="twitter" href=""><FaTwitter/></a>
+                        <a id="instagram" href=""><FaInstagram/></a>
+                    </div>
+                </div>
+            </div>
+        </IconContext.Provider>
+        
+    );
+}
--- src/App/pages/home/home.js
+++ src/App/pages/home/home.js
@@ -1,11 +1,13 @@
 import Card from "../../components/card/card";
 import RecommendedCard from "../../components/recommendedCard/recommendedCard";
+import Drawer from "../../components/drawer/drawer";
 
 export default function Home() {
     return (
         <main>
             <Card></Card>
             <RecommendedCard></RecommendedCard>
+            <Drawer></Drawer>
         </main>
     );
 }
