844ec43738a2540eb3b5d61b025738c33e8de39d
yaircs <yaircs16@gmail.com>
2022-10-26 18:40:31 -0500

    agregando boton mostrar password

--- src/App/components/input/input.js
+++ src/App/components/input/input.js
@@ -1,20 +1,29 @@
 import "./input.css";
 
+import cerraPassword from "../../../imgs/icons/mostrar.png";
+import { useState } from "react";
 import verPassword from "../../../imgs/icons/hidden.png";
 
 const Input = (props) => {
   const { isPassword, ...extra } = props;
+  const [shown, setShown] = useState(false);
+  const cambiar = () => {
+    console.log("un click");
+    setShown(!shown);
+  };
 
   return (
     <div className="input-container">
       {isPassword && (
-        <img
-          src={verPassword}
-          alt=""
-          className="verPassword"
-          width={"24px"}
-          height={"18px"}
-        />
+        <span onClick={cambiar}>
+          <img
+            src={shown ? cerraPassword : verPassword}
+            alt=""
+            className="verPassword"
+            width={"24px"}
+            height={"18px"}
+          />
+        </span>
       )}
       <input className="input" {...extra} />
     </div>
