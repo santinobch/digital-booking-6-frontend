edfa4b2f2ef6e3687d8ace07eb53f68929fa5e2d
Martin Rosas <mlrosas21@hotmail.com>
2022-10-27 11:34:06 -0300

    Agregando bloque categorias y editando estilos y props de cards

--- src/App/pages/home/home.js
+++ src/App/pages/home/home.js
@@ -3,13 +3,13 @@ import RecommendedCard from "../../components/recommendedCard/recommendedCard";
 import Drawer from "../../components/drawer/drawer";
 import Datepicker from "../../components/datepicker/datepicker";
 import Button from "../../components/button/button";
+import Categorias from "../../components/categorias/categorias";
 
 
 export default function Home() {
     return (
         <main>
-            <Card></Card>
-            <RecommendedCard></RecommendedCard>
+            <Categorias/>
             {/* <Drawer></Drawer> */}
             <Datepicker/>
             <Button style="light" width="300px">Light</Button>
