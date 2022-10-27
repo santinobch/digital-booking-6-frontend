2aab375ab002967ae19d9f53796255421512fadb
Maria Espinoza <mariaespinoza1994@gmail.com>
2022-10-26 22:54:57 -0300

    fixed search bar

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import Home from "./pages/home/home";
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import SearchBar from './components/inputs/select/SearchBar';
+import {SearchBar} from './components/inputs/select/SearchBar';
 
 function App() {
   return (
--- src/App/components/inputs/select/SearchBar.js
+++ src/App/components/inputs/select/SearchBar.js
@@ -1,36 +1,36 @@
 import React from "react";
+import Select from "react-select";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import Select from "react-select";
 
 const ciudades = [
-    { label: "San Carlos de Bariloche", value: "San Carlos de Bariloche" },
-    { label: "Buenos Aires", value: "Buenos Aires" },
-    { label: "Mendoza", value: "Mendoza" },
-    { label: "Córdoba", value: "Córdoba" }
-  ];
+  { label: "San Carlos de Bariloche", value: "San Carlos de Bariloche" },
+  { label: "Buenos Aires", value: "Buenos Aires" },
+  { label: "Mendoza", value: "Mendoza" },
+  { label: "Córdoba", value: "Córdoba" }
+];
 
 export const SearchBar = () => {
-    const handleSelectChange = ({ value }) => {
-      console.log(value);
-    };
-  
-    return (
-      <div>
-        <Select
-          placeholder={
-            <div >
-              <FontAwesomeIcon
-                icon={faLocationDot}
-                style={{ marginRight: "4px" }}
-              />
-              ¿A dónde vamos?
-            </div>
-          }
-          options={ciudades}
-          onChange={handleSelectChange}
-        />
-      </div>
-    );
+  const handleSelectChange = ({ value }) => {
+    console.log(value);
   };
-  
+
+  return (
+    <div className="placeholder-select">
+      <Select
+        placeholder={
+          <div className="SearchBar-container">
+            <FontAwesomeIcon
+              icon={faLocationDot}
+              style={{ marginRight: "4px" }}
+            />
+            ¿A dónde vamos?
+          </div>
+        }
+        options={ciudades}
+        onChange={handleSelectChange}
+      />
+    </div>
+  );
+};
+
