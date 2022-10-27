e572d92abd204516345aaa88d860a84a81c60128
Maria Espinoza <mariaespinoza1994@gmail.com>
2022-10-26 21:28:53 -0300

    select

--- src/App/components/inputs/select/select.js
+++ src/App/components/inputs/select/select.js
@@ -1,9 +1,37 @@
+import React from "react";
 import styles from "./drawer.module.scss";
+import Select from "react-select";
 
 document.getElementById("location").
 
-export default function Drawer() {
-    return (
+const ciudades = [
+    { label: "San Carlos de Bariloche", value: "San Carlos de Bariloche" },
+    { label: "Buenos Aires", value: "Buenos Aires" },
+    { label: "Mendoza", value: "Mendoza" },
+    { label: "Córdoba", value: "Córdoba" }
+  ];
 
+export const select = () => {
+    const handleSelectChange = ({ value }) => {
+      console.log(value);
+    };
+  
+    return (
+      <div>
+        <Select
+          placeholder={
+            <div >
+              <FontAwesomeIcon
+                icon={faLocationDot}
+                style={{ marginRight: "4px" }}
+              />
+              ¿A dónde vamos?
+            </div>
+          }
+          options={ciudades}
+          onChange={handleSelectChange}
+        />
+      </div>
     );
-}
+  };
+  
