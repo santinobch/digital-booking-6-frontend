658873dbcc86000b9d17f109dd6c6e076c8d0dd8
Martin Rosas <mlrosas21@hotmail.com>
2022-11-05 20:48:44 -0300

    Botón ver más

--- src/App/components/galeria/galeria.js
+++ src/App/components/galeria/galeria.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import './galeria.scss'
+import Modal from 'react-modal'
 
 
 const Galeria = () => {
+
+  const [modalOpen, setModalOpen] = React.useState(false);
+
+  function openModal() {
+    setModalOpen(true);
+  }
+
+  function closeModal(){
+    setModalOpen(false);
+  }
   
   const images = [
       'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -11,15 +22,22 @@ const Galeria = () => {
       'https://images.unsplash.com/photo-1563911302283-d2bc129e7570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
       'https://images.unsplash.com/photo-1552566626-2d907dab0dff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8aG90ZWwlMjBiYXJ8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60'
   ]
+
   return (
+    <>
+    <Modal isOpen={modalOpen} className="carouselModal">
+
+    </Modal>
     <section className='imageGallery'>
       <div className='mainImgContainer'>
-        <img className='mainImg' src={images[0]} alt="Main" />
+        <img className='mainImg' src={images[0]} alt="Main" onClick={openModal} />
       </div>
       <div className='otherImgsContainer'>
-        {images.slice(1).map(i => <img className='otherImg' src={i} alt="Other"/>)}
+        {images.slice(1).map((i, index) => <img key={index} className='otherImg' onClick={openModal} src={i} alt="Other"/>)}
+        <button className='verMasBtn' onClick={openModal}>Ver más</button>
       </div>
     </section>
+    </>
   )
 }
 
