6e2dda0332199aa896146ca62885ee4b8189f56a
Martin Rosas <mlrosas21@hotmail.com>
2022-11-05 19:41:31 -0300

    Galeria en desktop - sin botón ver más

--- src/App/pages/home/home.js
+++ src/App/pages/home/home.js
@@ -3,13 +3,15 @@ import styles from "./home.module.scss";
 import Buscador from "../../components/buscador/buscador";
 import Categorias from "../../components/categorias/categorias"
 import Listado from "../../components/listado/listado";
+import Galeria from "../../components/galeria/galeria";
 
 export default function Home() {
     return (
         <main className={styles.main}>
-            <Buscador/>
+            <Galeria/>
+            {/* <Buscador/>
             <Categorias/>
-            <Listado/>
+            <Listado/> */}
         </main>
     );
 }
