28ee8a922a542d26917ec70c4a4712e44b1ddab7
Maria Espinoza <mariaespinoza1994@gmail.com>
2022-11-06 16:24:41 -0300

    calendario responsive

--- src/App/components/calendario/calendar.js
+++ src/App/components/calendario/calendar.js
@@ -5,22 +5,37 @@ import "../calendario/calendar.scss";
 export default function Calendario() {
   const weekDays = ["D", "L", "M", "M", "J", "V", "S"];
   const width = window.innerWidth;
+  const months = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  ];
 
   return (
     <>
-      <section className="product--calendarLayout">
+        <section className="fechasDisponibles">
         <h2>Fechas disponibles</h2>
-        <div className="product--calendarLayout-elements">
+        <div className="contenedorCalendario">
           <div className="product--calendarLayout-container">
             <Calendar
               weekDays={weekDays}
               numberOfMonths={width >= 768 ? 2 : 2}
               minDate={new Date()}
               hideYear
+              months={months}
             />
           </div>
-          <div className="product--calendarLayout-form">
-            <h3>Agregá tus fechas de viaje para obtener precios exactos</h3>
+          <div className="contendorReserva">
+            <h4>Agregá tus fechas de viaje para obtener precios exactos</h4>
             <button className="button">Iniciar reserva</button>
           </div>
         </div>
