99ef1c7d6feaeca5c3592b9ba0aad653592d43a6
Martin Rosas <mlrosas21@hotmail.com>
2022-11-08 22:07:45 -0300

    Cambiando endpoints en service

--- src/App/services/index.js
+++ src/App/services/index.js
@@ -1,9 +1,10 @@
 const urlBase = process.env.REACT_APP_API_URL;
 
 export function getCiudades() {
-  return fetch(`${urlBase}/ciudades`).then((respuesta) => respuesta.json());
+  return fetch(`${urlBase}/ciudades/all`).then((respuesta) => respuesta.json());
 }
 
+// 
 export function getHospedaje(busqueda) {
   const query = new URLSearchParams(busqueda);
   return fetch(`${urlBase}/hospedajes?${query}`).then((respuesta) =>
@@ -12,9 +13,10 @@ export function getHospedaje(busqueda) {
 }
 
 export function getCategorias() {
-  return fetch(`${urlBase}/categorias`).then((respuesta) => respuesta.json());
+  return fetch(`${urlBase}/categorias/all`).then((respuesta) => respuesta.json());
 }
 
+// Estos se renderizan en el home
 export function getRandomProductos(){
   return fetch(`${urlBase}/productos/random`).then((respuesta) => respuesta.json())
 }
