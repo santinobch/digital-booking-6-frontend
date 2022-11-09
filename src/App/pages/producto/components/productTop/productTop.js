1ffe3af7be1793c3f5163521e3c4bf30781a9f52
Santino <santino.bch@gmail.com>
2022-11-08 22:11:19 -0300

    estilo general terminado

--- src/App/pages/producto/components/productTop/productTop.js
+++ src/App/pages/producto/components/productTop/productTop.js
@@ -1,5 +1,6 @@
 import styles from "./productTop.module.scss";
 import star from "../../../../../imgs/icons/star.png";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 export default function ProductTop() {
     return (
@@ -13,26 +14,31 @@ export default function ProductTop() {
             </section>
             <section className={styles.productLocation}>
 
-                <div>
+                <div className={styles.location}>
                     <p>
                         Buenos Aires, Ciudad Autónoma de Buenos Aires, Argentina a 940 m del centro 
                     </p>
                 </div>
 
-                <div>
-                    <p>Muy bueno</p>
+                <div className={styles.calificacion}>
+                    
 
-                    <div className={styles.starsImg}>
-                        <img src={star} alt=""></img>
-                        <img src={star} alt=""></img>
-                        <img src={star} alt=""></img>
-                        <img src={star} alt=""></img>
-                        <img src={star} alt=""></img>
+                    <div className={styles.textoStars}>
+
+                        <p>Muy bueno</p>
+
+                        <div className={styles.starsImg}>
+                            <img src={star} alt=""></img>
+                            <img src={star} alt=""></img>
+                            <img src={star} alt=""></img>
+                            <img src={star} alt=""></img>
+                            <img src={star} alt=""></img>
+                        </div>
                     </div>
 
-                    <div className={styles.calificacion}>
+                    <div className={styles.puntaje}>
                         <span>8</span>
-                    </div>
+                    </div>                
                 </div>
                 
             </section>
@@ -42,10 +48,62 @@ export default function ProductTop() {
             <section className={styles.productInfo}>
                 <h1>Alójate en el corazón de Buenos Aires</h1>
 
-                <p>Está situado a solo unas calles de la avenida Alvear, de la avenida Quintana, del parque San Martín y del distrito de Recoleta. En las inmediaciones también hay varios lugares de interés, como la calle Florida, el centro comercial Galerías Pacífico, la zona de Puerto Madero, la plaza de Mayo y el palacio Municipal.</p>
+                <p>
+                    Está situado a solo unas calles de la avenida Alvear, de la avenida Quintana, del parque San Martín y del distrito de Recoleta. En las inmediaciones también hay varios lugares de interés, como la calle Florida, el centro comercial Galerías Pacífico, la zona de Puerto Madero, la plaza de Mayo y el palacio Municipal.
+                </p>
                 
+                <p>
+                    Nuestros clientes dicen que esta parte de Buenos Aires es su favorita, según los comentarios independientes.
+                </p>
+                
+                <p>
+                    El Hotel es un hotel sofisticado de 4 estrellas que goza de una ubicación tranquila, a poca distancia de prestigiosas galerías de arte, teatros, museos y zonas comerciales. Además, hay WiFi gratuita. El establecimiento sirve un desayuno variado de 07:00 a 10:30.
+                </p>
+
             </section>
             <section className={styles.productOfferings}>
+                <h1>
+                    ¿Qué ofrece este lugar?
+                </h1>
+
+                <hr/>
+
+                <div className={styles.offeringsContainer}>
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+                    <div className={styles.offering}>
+                        <FaRegEyeSlash style={{ color: "#31363F", fontSize:"24px" }}/>
+                        <p>Test</p>
+                    </div>
+
+                </div>
                 
             </section>
         </>
--- src/App/pages/producto/producto.js
+++ src/App/pages/producto/producto.js
@@ -1,12 +1,16 @@
 import styles from "./producto.module.scss";
 
 import ProductTop from "./components/productTop/productTop";
+import ProductBottom from "./components/productBottom/productBottom";
 
 
 export default function Producto() {
     return (
         <main className={styles.main}>
             <ProductTop></ProductTop>
+
+            <ProductBottom></ProductBottom>
+            
         </main>
     );
 }
