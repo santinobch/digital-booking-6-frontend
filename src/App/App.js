800414a92f9d732cdcf1bd95454fc045d3759226
Martin Rosas <mlrosas21@hotmail.com>
2022-11-10 13:32:08 -0300

    Agregando enrutador /home

--- src/App/App.js
+++ src/App/App.js
@@ -26,7 +26,7 @@ const router = createBrowserRouter([{
     element: <Root />,
     children: [
         {
-            path: "",
+            path: "/home",
             element: <Home />,
         },
         {
