783864e23d02b885d51e58f7937fe5c4fdd143c2
yaircs <yaircs16@gmail.com>
2022-12-07 19:09:01 -0500

    agregando comp succesfull de producto

--- src/App/pages/booking/components/detalleBooking/detalleBooking.js
+++ src/App/pages/booking/components/detalleBooking/detalleBooking.js
@@ -1,77 +1,80 @@
-import styles from "./detalleBooking.module.scss";
-
-import star from "../../../../../imgs/icons/star.png";
-
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { postBooking } from "../../../../services/bookings";
-import { useNavigate } from 'react-router-dom';
-
-import SpinnerLoader from "../../../../components/spinnerLoader/spinnerLoader";
-
 import Button from "../../../../components/button/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SpinnerLoader from "../../../../components/spinnerLoader/spinnerLoader";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { postBooking } from "../../../../services/bookings";
+import star from "../../../../../imgs/icons/star.png";
+import styles from "./detalleBooking.module.scss";
+import { useNavigate } from "react-router-dom";
 
-export default function DetalleBooking({auth, product, fechas, usuario}) {
-    const navigate = useNavigate();
+export default function DetalleBooking({ auth, product, fechas, usuario }) {
+  const navigate = useNavigate();
 
-    const handleConfirmarBooking = () => {
-        postBooking(auth, usuario, product, fechas).then(status => {
-            if(status>200 && status<300){
-                navigate("/succesfulBooking")
-            }
-        })
-    }
+  const handleConfirmarBooking = () => {
+    postBooking(auth, usuario, product, fechas).then((status) => {
+      if (status > 200 && status < 300) {
+        navigate("/succesfull?page=reserva-exitosa");
+      }
+    });
+  };
 
-    if(product.length === 0){
-        return (
-            <SpinnerLoader/>
-        )
-    } else {
-        return (
-            <div className={styles.inputContainer}>
-    
-                <h3 className={styles.title}>Detalle de la booking</h3>
-    
-                <div className={styles.tabletContainer}>
-                    <img src={product.imagenes[0].imagenUrl} className={styles.cuartoImg} alt=''></img>
-    
-                    <div className={styles.bottomContainer}>
-                        <h4>{product.categoryNombre}</h4>
-                        <h3>{product.titulo}</h3>
-                        <div className={styles.stars}>
-                            <img src={star} alt=""></img>
-                            <img src={star} alt=""></img>
-                            <img src={star} alt=""></img>
-                            <img src={star} alt=""></img>
-                            <img src={star} alt=""></img>
-                        </div>
-    
-                        <div className={styles.ubication}>
-                            <FontAwesomeIcon icon={faLocationDot} style={{ marginRight: "4px" }} />
-                            <p>{product.ciudadNombre}, {product.ciudadPais}</p>
-                        </div>
-                        
-                        <hr />
-    
-                        <div className={styles.dateInput}>
-                            <p>Check in</p>
-                            <p className={styles.date}>{fechas.fechaCheckIn}</p>
-                        </div>
-    
-                        <hr />
-    
-                        <div className={styles.dateInput}>
-                            <p>Check out</p>
-                            <p className={styles.date}>{fechas.fechaCheckOut}</p>
-                        </div>
-    
-                        <hr />
-    
-                        <Button styleBtn="dark" onClick={handleConfirmarBooking}>Confirmar booking</Button>
-                    </div>
-                </div>
-                
+  if (product.length === 0) {
+    return <SpinnerLoader />;
+  } else {
+    return (
+      <div className={styles.inputContainer}>
+        <h3 className={styles.title}>Detalle de la booking</h3>
+
+        <div className={styles.tabletContainer}>
+          <img
+            src={product.imagenes[0].imagenUrl}
+            className={styles.cuartoImg}
+            alt=""
+          ></img>
+
+          <div className={styles.bottomContainer}>
+            <h4>{product.categoryNombre}</h4>
+            <h3>{product.titulo}</h3>
+            <div className={styles.stars}>
+              <img src={star} alt=""></img>
+              <img src={star} alt=""></img>
+              <img src={star} alt=""></img>
+              <img src={star} alt=""></img>
+              <img src={star} alt=""></img>
             </div>
-        );
-    }
+
+            <div className={styles.ubication}>
+              <FontAwesomeIcon
+                icon={faLocationDot}
+                style={{ marginRight: "4px" }}
+              />
+              <p>
+                {product.ciudadNombre}, {product.ciudadPais}
+              </p>
+            </div>
+
+            <hr />
+
+            <div className={styles.dateInput}>
+              <p>Check in</p>
+              <p className={styles.date}>{fechas.fechaCheckIn}</p>
+            </div>
+
+            <hr />
+
+            <div className={styles.dateInput}>
+              <p>Check out</p>
+              <p className={styles.date}>{fechas.fechaCheckOut}</p>
+            </div>
+
+            <hr />
+
+            <Button styleBtn="dark" onClick={handleConfirmarBooking}>
+              Confirmar booking
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/App/pages/createProduct/createProduct.js
+++ src/App/pages/createProduct/createProduct.js
@@ -1,61 +1,152 @@
+import Button from "../../components/button/button";
+import Input from "../../components/inputs/text/input";
+import ProductHeader from "../../components/productHeader/productHeader";
+import { SelectInput } from "../../components/inputs/selectTest/select";
+import Textarea from "../../components/textarea/textarea";
 import styles from "./createProduct.module.scss";
-
+import { text } from "@fortawesome/fontawesome-svg-core";
+import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
 //Components
-import ProductHeader from "../../components/productHeader/productHeader";
-import Input from "../../components/inputs/text/input";
-import Textarea from "../../components/textarea/textarea";
-import { SelectInput } from "../../components/inputs/selectTest/select";
 
 //Services
-
+const styleMessageError = {
+  marginTop: "-15px",
+  color: "red",
+  fontWeight: "400",
+  fontSize: ".85rem",
+  textAlign: "right",
+};
 
 export default function CreateProduct() {
-    
-    return (
-        <main className={styles.main}>
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState(null);
 
-            <ProductHeader titulo="Administración"></ProductHeader>
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const values = Object.fromEntries(formData.entries());
 
-            <h3>Crear propiedad</h3>
-            <div className={styles.propiedadContainer}>
-                <div className={styles.inputsContainer}>
-                    <Input label="Nombre de la propiedad" placeholder="Hermirage Hotel"></Input>
-                    {/* <Input label="Categoría" placeholder="Hotel"></Input> */}
-                    <SelectInput></SelectInput>   
-                    <Input label="Dirección" placeholder="Av. Colón 1643"></Input>
-                    {/* <Input label="Ciudad" placeholder="Ciudad"></Input> */}
-                    <SelectInput></SelectInput>
-                </div>
+    // validaciones
+    let fieldErrors = {};
+    for (const key in values) {
+      if (!values[key]) {
+        fieldErrors[key] = "Este campo es requerido";
+      }
+    }
 
-                <div className={styles.atributosContainer}>
-                    <h4>Agregar atributos</h4>
-                    <Input label="Descripción" placeholder="Escriba aquí"></Input>
-                </div>
-                
-                <div className={styles.politicaContainer}>
-                    <h4>Políticas del producto</h4>
-                
-                    <div className={styles.politica}>
-                        <h5>Normas de la casa</h5>
-                        <Textarea label="Descripción" placeholder="Escriba aquí"></Textarea>
-                    </div>
-                    <div className={styles.politica}>
-                        <h5>Salud y seguridad</h5>
-                        <Textarea label="Descripción" placeholder="Escriba aquí"></Textarea>
-                    </div>
-                    <div className={styles.politica}>
-                        <h5>Política de cancelación</h5>
-                        <Textarea label="Descripción" placeholder="Escriba aquí"></Textarea>
-                    </div>
-                </div>
+    if (Object.keys(fieldErrors).length > 0) {
+      setErrors(fieldErrors);
+      return;
+    }
 
-                <h4>Cargar imágenes</h4>
-                <Input placeholder="Escriba la URL de su imagen"></Input>
+    // guardando datos
+    navigate("/succesfull?page=create-product");
+  }
+
+  function handleKeyUp(e) {
+    const { name } = e.target;
+    setErrors({ ...errors, [name]: "" });
+  }
+
+  return (
+    <main className={styles.main}>
+      <ProductHeader titulo="Administración"></ProductHeader>
+      <h3>Crear propiedad</h3>
+      <form method="POST" onSubmit={handleSubmit}>
+        <div className={styles.propiedadContainer}>
+          <div className={styles.inputsContainer}>
+            <Input
+              label="Nombre de la propiedad"
+              placeholder="Hermirage Hotel"
+              name="nameP"
+              type="text"
+              onKeyUp={handleKeyUp}
+              error={!!errors?.nameP}
+            />
+            <p style={styleMessageError}>{errors?.nameP}</p>
+            {/* <Input label="Categoría" placeholder="Hotel"></Input> */}
+            <SelectInput></SelectInput>
+            <Input
+              label="Dirección"
+              placeholder="Av. Colón 1643"
+              name="direccion"
+              type="text"
+              onKeyUp={handleKeyUp}
+              error={!!errors?.direccion}
+            />
+            <p style={styleMessageError}>{errors?.direccion}</p>
+            {/* <Input label="Ciudad" placeholder="Ciudad"></Input> */}
+            <SelectInput></SelectInput>
+          </div>
+
+          <div className={styles.atributosContainer}>
+            <h4>Agregar atributos</h4>
+            <Input
+              label="Descripción"
+              placeholder="Escriba aquí"
+              name="descripcion"
+              type="text"
+              onKeyUp={handleKeyUp}
+              error={!!errors?.descripcion}
+            />
+            <p style={styleMessageError}>{errors?.descripcion}</p>
+          </div>
+
+          <div className={styles.politicaContainer}>
+            <h4>Políticas del producto</h4>
+
+            <div className={styles.politica}>
+              <h5>Normas de la casa</h5>
+              <Textarea
+                label="Descripción"
+                placeholder="Escriba aquí"
+                name="normasDeCasa"
+                onKeyUp={handleKeyUp}
+                error={!!errors?.normasDeCasa}
+              />
+              <p style={styleMessageError}>{errors?.normasDeCasa}</p>
             </div>
+            <div className={styles.politica}>
+              <h5>Salud y seguridad</h5>
+              <Textarea
+                label="Descripción"
+                placeholder="Escriba aquí"
+                name="saludSeguridad"
+                onKeyUp={handleKeyUp}
+                error={!!errors?.saludSeguridad}
+              />
+              <p style={styleMessageError}>{errors?.saludSeguridad}</p>
+            </div>
+            <div className={styles.politica}>
+              <h5>Política de cancelación</h5>
+              <Textarea
+                label="Descripción"
+                placeholder="Escriba aquí"
+                name="politicaCancelacion"
+                onKeyUp={handleKeyUp}
+                error={!!errors?.politicaCancelacion}
+              />
+              <p style={styleMessageError}>{errors?.politicaCancelacion}</p>
+            </div>
+          </div>
 
-        </main>
-    );
+          <h4>Cargar imágenes</h4>
+          <Input
+            placeholder="Escriba la URL de su imagen"
+            name="urlImagen"
+            type="text"
+            onKeyUp={handleKeyUp}
+            error={!!errors?.urlImagen}
+          />
+          <p style={styleMessageError}>{errors?.urlImagen}</p>
+        </div>
+        <Button type="submit" styleBtn="dark" width="20%">
+          Crear
+        </Button>
+      </form>
+    </main>
+  );
 }
