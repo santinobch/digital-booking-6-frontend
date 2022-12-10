ac94e97b6347a7e2d56923c283cdd71bac24cc1b
Santino <santino.bch@gmail.com>
2022-12-10 15:49:37 -0300

    Estilos del select de home adecuados

--- src/App/components/inputs/select/SearchBar.js
+++ src/App/components/inputs/select/SearchBar.js
@@ -8,66 +8,67 @@ import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { getCiudades } from "../../../services/cities";
 
 const formatOptionLabel = ({ label, country }) => (
-  <div style={{ display: "flex", alignItems: "center" }}>
-    <FontAwesomeIcon icon={faLocationDot} style={{ marginRight: "14px" }} />
-    <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-      <p style={{ marginBottom: "0px" }}>{label}</p>
-      <p style={{ color: "#ccc", marginBottom: "0px" }}>{country}</p>
+    <div style={{ display: "flex", alignItems: "center" }}>
+        <FontAwesomeIcon icon={faLocationDot} style={{ marginRight: "14px" }} />
+        <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
+            <p style={{ marginBottom: "0px" }}>{label}</p>
+            <p style={{ color: "#ccc", marginBottom: "0px" }}>{country}</p>
+        </div>
     </div>
-  </div>
 );
 
 const formatOptionLabelCreation = ({ label, country }) => (
-      <p style={{ marginBottom: "0px" }}>{label}, {country}</p>
+    <p style={{ marginBottom: "0px" }}>{label}, {country}</p>
 );
 
 export const SelectSearch = ({ onChange, home }) => {
-  const [ciudades, setCiudades] = useState([]);
+    const [ciudades, setCiudades] = useState([]);
 
-  useEffect(() => {
-    getCiudades().then((datos) => setCiudades(datos));
-  }, []);
+    useEffect(() => {
+        getCiudades().then((datos) => setCiudades(datos));
+    }, []);
 
-  const handleSelectChange = ({ value }) => {
-    if (value === "Todos") {
-      onChange("");
-    } else {
-      onChange({ name: "ciudad", value, category: "" });
-    }
-  };
-
-  return (
-    <div className="placeholder-select">
-      {home ?       
-      <Select
-        placeholder={
-          <div className="SearchBar-container">
-            <FontAwesomeIcon
-              icon={faLocationDot}
-              style={{ marginRight: "4px" }}
-            />
-            ¿A dónde vamos?
-          </div>
+    const handleSelectChange = ({ value }) => {
+        if (value === "Todos") {
+        onChange("");
+        } else {
+        onChange({ name: "ciudad", value, category: "" });
         }
-        options={ciudades.map((opc) => ({
-          value: opc.id,
-          country: opc.pais,
-          label: opc.nombre,
-        }))}
-        onChange={handleSelectChange}
-        formatOptionLabel={formatOptionLabel}
-      /> : 
-      <Select
-      placeholder="Ciudad"
-      options={ciudades.map((opc) => ({
-        value: opc.id,
-        country: opc.pais,
-        label: opc.nombre,
-      }))}
-      onChange={handleSelectChange}
-      formatOptionLabel={formatOptionLabelCreation}
-    />}
+    };
 
-    </div>
-  );
+    return (
+        <div className="placeholder-select">
+            {home ?       
+                <Select
+                    classNamePrefix="selectBooking"
+                    placeholder={
+                    <div className="SearchBar-container">
+                        <FontAwesomeIcon
+                        icon={faLocationDot}
+                        style={{ marginRight: "4px" }}
+                        />
+                        ¿A dónde vamos?
+                    </div>
+                    }
+                    options={ciudades.map((opc) => ({
+                    value: opc.id,
+                    country: opc.pais,
+                    label: opc.nombre,
+                    }))}
+                    onChange={handleSelectChange}
+                    formatOptionLabel={formatOptionLabel}
+                /> : 
+                <Select
+                    placeholder="Ciudad"
+                    options={ciudades.map((opc) => ({
+                        value: opc.id,
+                        country: opc.pais,
+                        label: opc.nombre,
+                    }))}
+                onChange={handleSelectChange}
+                formatOptionLabel={formatOptionLabelCreation}
+            />}
+
+        </div>
+    );
 };
