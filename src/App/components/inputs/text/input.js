2b2ae4a2ea67a0213b2de6e00a8c34c96d2fad44
Santino <santino.bch@gmail.com>
2022-12-11 02:20:00 -0300

    Revisando input

--- src/App/components/inputs/text/input.js
+++ src/App/components/inputs/text/input.js
@@ -4,97 +4,93 @@ import { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 export default function Input(props) {
-  const id = uuid();
-  let showPassButton = "none";
-  let showSubLabel_ = "none";
-  const [showSubLabel, setSubLabelVisibility] = useState(showSubLabel_);
-  const [invalid, setInvalid] = useState(false);
+    const id = uuid();
+    let showPassButton = "none";
+    let showSubLabel_ = "none";
+    const [showSubLabel, setSubLabelVisibility] = useState(showSubLabel_);
+    const [invalid, setInvalid] = useState(false);
 
-  let toggle_ = false;
-  const [toggleState, setToggle] = useState(toggle_);
+    const [toggleState, setToggle] = useState(false);
 
-  if (props.type === "password") {
-    showPassButton = "block";
-  }
-
-  function keyUp(event) {
-    if (!document.getElementById(id).checkValidity()) {
-      setSubLabelVisibility("block");
-    } else {
-      setSubLabelVisibility("none");
+    if (props.type === "password") {
+        showPassButton = "block";
     }
 
-    props?.setHasError?.(false);
-
-    if (props.name === "password_confirm") {
-      let confirmValue = event.target.value;
-      if (
-        props.comparePass !== undefined &&
-        props.comparePass === confirmValue
-      ) {
-        setSubLabelVisibility("none");
-        setInvalid(false);
-      } else {
-        setSubLabelVisibility("block");
-        setInvalid(true);
-      }
-    }
-  }
-  const handleChange = (event) => {
-    if (props.handlePass !== undefined) {
-      props.handlePass(event.target.value);
-    }
-  };
-  return (
-    <div className={styles.inputContainer} style={{ width: props.width }}>
-      <label className={styles.label} htmlFor={props.name}>
-        {props.label}
-      </label>
-
-      <input
-        id={id}
-        className={`${styles.input} ${invalid ? styles.invalidInput : null}`}
-        style={{ width: props.width }}
-        src={props.src}
-        type={
-          props.type === "password"
-            ? toggleState
-              ? "text"
-              : "password"
-            : props.type
+    function keyUp(event) {
+        if (!document.getElementById(id).checkValidity()) {
+            setSubLabelVisibility("block");
+        } else {
+            setSubLabelVisibility("none");
         }
-        onBlur={props.onBlur}
-        name={props.name}
-        onChange={props.onChange}
-        value={props.value}
-        placeholder={props.placeholder}
-        disabled={props.disabled}
-        checked={props.checked}
-        required={props.required}
-        pattern={props.pattern}
-        onKeyUp={(event) =>
-          props.onKeyUp ? props.onKeyUp(event) : keyUp(event)
+
+        props?.setHasError?.(false);
+
+        if (props.name === "password_confirm") {
+            let confirmValue = event.target.value;
+            if (
+                props.comparePass !== undefined &&
+                props.comparePass === confirmValue
+            ) {
+                setSubLabelVisibility("none");
+                setInvalid(false);
+            } else {
+                setSubLabelVisibility("block");
+                setInvalid(true);
+            }
         }
-      >
-        {props.children}
-      </input>
+    }
+    const handleChange = (event) => {
+        if (props.handlePass !== undefined) {
+            props.handlePass(event.target.value);
+        }
+    };
 
-      <label
-        className={styles.subLabel}
-        htmlFor={props.name}
-        style={{ display: showSubLabel }}
-      >
-        {props.subLabel}
-      </label>
+    return (
+        <div className={styles.inputContainer} style={{ width: props.width }}>
+        <label className={styles.label} htmlFor={props.name}>
+            {props.label}
+        </label>
 
-      <button
-        type="button"
-        style={{ display: showPassButton }}
-        className={styles.showPassButton}
-        onClick={() => setToggle(!toggleState)}
-      >
-        <FaRegEyeSlash style={{ color: "#31363F", fontSize: "24px" }} />
-      </button>
-    </div>
-  );
+        <input
+            id={id}
+            className={`${styles.input} ${invalid ? styles.invalidInput : null}`}
+            style={{ width: props.width }}
+            src={props.src}
+            type={
+                props.type === "password" ? ( toggleState ? "text" : "password") : props.type
+            }
+            onBlur={props.onBlur}
+            name={props.name}
+            onChange={props.onChange}
+            value={props.value}
+            placeholder={props.placeholder}
+            disabled={props.disabled}
+            checked={props.checked}
+            required={props.required}
+            pattern={props.pattern}
+            onKeyUp={(event) =>
+                props.onKeyUp ? props.onKeyUp(event) : keyUp(event)
+            }
+        >
+            {props.children}
+        </input>
+
+        <label
+            className={styles.subLabel}
+            htmlFor={props.name}
+            style={{ display: showSubLabel }}
+        >
+            {props.subLabel}
+        </label>
+
+        <button
+            type="button"
+            style={{ display: showPassButton }}
+            className={styles.showPassButton}
+            onClick={() => setToggle(!toggleState)}
+        >
+            <FaRegEyeSlash style={{ color: "#31363F", fontSize: "24px" }} />
+        </button>
+        </div>
+    );
 }
